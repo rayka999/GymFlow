05288f7b33ad4c4c4a0d0c8ff1bc980aa04cf4bc
rayka999 <raykalgarcia07gmail.com>
2025-12-19 14:44:06 -0300

    pag-sobre e pag-contato, prototipos para outras pags de aluno

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', function(req, res) {
   res.render('pag-inicial');
 });
 
+router.get('/sobre', function(req,res){
+  res.render('pag-sobre')
+});
+
+router.get('/contato', function (req,res) {
+  res.render('pag-contato')
+})
+
 module.exports = router;
