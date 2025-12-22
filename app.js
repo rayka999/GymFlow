8efa83b8a697853be2451a8292c797415d6ccf8e
rayka999 <raykalgarcia07@gmail.com>
2025-12-22 10:16:46 -0300

    prototipos de instrutor

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usuarioRouter= require ('./routes/usuario');
-var exercicioRouter= require('./routes/exercicio')
+var exercicioRouter= require('./routes/exercicio');
+var instrutorRouter= require ('./routes/instrutor');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/usuario',usuarioRouter);
 app.use('/exercicio',exercicioRouter);
+app.use('/instrutor',instrutorRouter);
 app.use('/usuario/exercicio', exercicioRouter);
 
 // catch 404 and forward to error handler
--- routes/instrutor.js
+++ routes/instrutor.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const router = express.Router();
-const instrutorAuth = require('../middlewares/instrutorAuth');
+var router = express.Router();
+const instrutorAuth = require('../middlewares/instrutor_auth');
 
-router.get('/', instrutorAuth, (req, res) => {
+router.get('/inicio', instrutorAuth, (req, res) => {
     res.render('instrutor-home', {
         usuario: req.session.usuario
     });
